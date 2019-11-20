94af797c4a6c3f77620a1a8d080c89059ee2087b
nateschillen <54914760+nateschillen@users.noreply.github.com>
2019-11-19 23:28:32 -0700

    initial push

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyDvpGVFk30U3lhefZX-mGCjvQiXSddH25s",
-  authDomain: "authentication-c398a.firebaseapp.com",
-  databaseURL: "https://authentication-c398a.firebaseio.com",
-  projectId: "authentication-c398a",
-  storageBucket: "",
-  messagingSenderId: "815408671129",
-  appId: "1:815408671129:web:220f040305c6f8bae6cb08"
+  apiKey: "AIzaSyARy7WERt2ydPhsUgwvAEMTw8ljBnJH72w",
+  authDomain: "finalproject-cafc4.firebaseapp.com",
+  databaseURL: "https://finalproject-cafc4.firebaseio.com",
+  projectId: "finalproject-cafc4",
+  storageBucket: "finalproject-cafc4.appspot.com",
+  messagingSenderId: "426971232569",
+  appId: "1:426971232569:web:006129ae5a2bbfec5ad0e8"
 };
 
 firebase.initializeApp(config);
